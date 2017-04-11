c643d015cb7841144a6fe591342d2fb671b4e7da
patrick henry <bitbucket@patrickhenry.co.uk>
2017-04-11 00:49:38 -0400

    navs

--- source/vlogplanner/resources/views/includes/navigation.blade.php
+++ source/vlogplanner/resources/views/includes/navigation.blade.php
@@ -5,31 +5,11 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <a class="navbar-brand" href="#">
-         <img src="http://marketing.rakuten.com/img/common/logo/2.gif" width="150"   align="absmiddle" style="float:left;">
+         Vlog Planner
         </a>
         <div class="collapse navbar-collapse" id="navbarNav1">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link waves-effect waves-light">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link waves-effect waves-light">Features</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link waves-effect waves-light">Pricing</a>
-                </li>
-                <li class="nav-item dropdown btn-group waves-effect waves-light">
-                    <a class="nav-link dropdown-toggle waves-effect waves-light" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                    <div class="dropdown-menu dropdown" aria-labelledby="dropdownMenu1">
-                        <a class="dropdown-item waves-effect waves-light">Action</a>
-                        <a class="dropdown-item waves-effect waves-light">Another action</a>
-                        <a class="dropdown-item waves-effect waves-light">Something else here</a>
-                    </div>
-                </li>
-            </ul>
-            <form class="form-inline waves-effect waves-light">
-                <input class="form-control" type="text" placeholder="Search">
-            </form>
+            
+            
         </div>
     </div>
 </nav>
