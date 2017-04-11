1ea867c3660b9b110d6637f21d6a07dada132b0b
patrick henry <bitbucket@patrickhenry.co.uk>
2017-04-11 01:48:00 -0400

    using

--- source/vlogplanner/resources/views/entries.blade.php
+++ source/vlogplanner/resources/views/entries.blade.php
@@ -5,38 +5,38 @@
 
 @include('includes.menu')
 
- @include('includes.modaladdform')
 
 <div class="app-content" id="data-items"  >
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+    <div class="container" ng-app="" ng-controller="customersController">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Dashboard</div>
 
-<table>
-  <tr ng-repeat="x in entries">
-    <td>@{{ x.id }}</td>
-    <td>@{{ x.title }}</td>
-  </tr>
-</table>
-
-
-
-
-                <div class="panel-body">
+                    <table>
+                        <tr ng-repeat="x in entries">
+                            <td>@{{ x.id }}</td>
+                            <td>@{{ x.title }}</td>
+                        </tr>
+                    </table>
 
 
 
 
+                    <div class="panel-body">
 
-</div>
+
+
+
+
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div>
+
+@include('includes.modaladdform')
 
 
-
-@endsection
+    @endsection
